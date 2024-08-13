3786b65aef2351124b0bc4b832269e3a3ef4e49a
Harish Reddy <harish438@users.noreply.github.com>
2024-08-13 09:04:10 +0530

    Update DateAdder.cs
    added some validations for test case scenarios

--- DateAdder.cs
+++ DateAdder.cs
@@ -1,6 +1,8 @@
 // code for displaying the output date in dd/mm/yyyy format along with a number of days to add to the input date.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DateAdder
 {
@@ -10,20 +12,79 @@ namespace DateAdder
         {
             // Array of number of days in all months in a year
             int[] totalDaysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-            
+
             // Input date
-            Console.WriteLine("Enter the date in dd/mm/yyyy format: ");
+            Console.Write("Enter the date in dd/mm/yyyy format: ");
             string inputDate = Console.ReadLine();
 
+            string[] splitDate = inputDate.Split('/');
+            
+            // check whether entered input day is numerical value
+            var isValid = int.TryParse(splitDate[0], out int day);
+            if (!isValid)
+            {
+                Console.Write("Please enter the valid day with numeric value..");
+                Console.ReadLine();
+                return;
+            }
+            
+            // check whether entered input month is numerical value
+            isValid = int.TryParse(splitDate[1], out int month);
+            if (!isValid)
+            {
+                Console.Write("Please enter the valid month with numeric value..");
+                Console.ReadLine();
+                return;
+            }
+           
+           // check whether entered input year is numerical value
+            isValid = int.TryParse(splitDate[2], out int year);
+            if (!isValid)
+            {
+                Console.Write("Please enter the valid year with numeric value..");
+                Console.ReadLine();
+                return;
+            }
+            
+            // check whether entered input year is valid
+            isValid = IsYearValid(year);
+            if (!isValid)
+            {
+                Console.Write("Please enter the valid year..");
+                Console.ReadLine();
+                return;
+            }
+            
+            // check whether entered input month is valid
+            var isMonthValid = IsMonthValid(month);
+            if (!isMonthValid)
+            {
+                Console.Write("Please enter the valid month..");
+                Console.ReadLine();
+                return;
+            }
+            
+            // check whether entered input day is valid
+            var isDayValid = IsDayValid(day, month, year);
+            if(!isDayValid)
+            {
+                Console.Write("Please enter the valid day..");
+                Console.ReadLine();
+                return;
+            }
+            
             // Input days 
-            Console.WriteLine("Enter the number of days: ");
+            Console.Write("Enter the number of days: ");
             string numberOfDays = Console.ReadLine();
 
-            string[] splitDate = inputDate.Split('/');
-            int day = int.Parse(splitDate[0]);
-            int month = int.Parse(splitDate[1]);
-            int year = int.Parse(splitDate[2]);
-            int daysToAdd = int.Parse(numberOfDays);
+            // check whether entered number of days is numerical value
+            isValid = int.TryParse(numberOfDays, out int daysToAdd);
+            if(!isValid)
+            {
+                Console.Write("Please enter the valid number of days with numeric value..");
+                Console.ReadLine();
+                return;
+            }
 
             // Add number of days to current days
             day += daysToAdd;
@@ -56,24 +117,70 @@ namespace DateAdder
             }
 
             // Output
-            Console.WriteLine($"The new date is: {day:D2}/{month:D2}/{year}");
+            Console.WriteLine($"The output date in dd/mm/yyyy format: {day:D2}/{month:D2}/{year:D4}");
+        }
+        
+        public static List<int> GetMonthWith31Days()
+        {
+            return new List<int>() { 1, 3, 5, 7, 8, 10, 12 };
+        }
+        
+        public static List<int> GetMonthWith30Days()
+        {
+            return new List<int>() { 4, 6, 9, 11 };
+        }
+        static bool IsDayValid(int day, int month, int year)
+        {
+            bool isMonthHaving31Days = GetMonthWith31Days().Any(x => x == month);
+            bool isMonthHaving30Days = GetMonthWith30Days().Any(x => x == month);
+            
+            if(isMonthHaving31Days)
+            {
+                return day <= 31;
+            }
+            if (isMonthHaving30Days)
+            {
+                return day <= 30;
+            }
+            if (month == 2 && IsLeapYear(year))
+            {
+                return day <= 29;
+            }
+            if (month == 2 && !IsLeapYear(year))
+            {
+                return day <= 28;
+            }
+            return false; ;
+        }
+        
+        static bool IsMonthValid(int month)
+        {
+            return month > 0 && month <= 12;
+        }
+        
+        static bool IsYearValid(int year)
+        {
+            return year > 0 && year <= 9999;
         }
         
         // Function to check if a year is a leap year
         static bool IsLeapYear(int year)
         {
             bool leap_year = false;
-            if (year % 4 == 0){
+            if (year % 4 == 0)
+            {
                 leap_year = true;
             }
-            if (year % 100 == 0){
+            if (year % 100 == 0)
+            {
                 leap_year = false;
             }
-            if (year % 400 == 0){
+            if (year % 400 == 0)
+            {
                 leap_year = true;
             }
             return leap_year;
         }
-        
+
     }
 }
